d0b176faf561fca844bc039b5f1db5c097759042
hbalmes56 <hadley-rae.balmes@mymail.champlain.edu>
2024-02-01 21:38:20 -0500

    commit for week2

---
+++ Week2/ProcessManagement4.ps1
@@ -0,0 +1,9 @@
+﻿$chrome = Get-Process -Name chrome -ErrorAction SilentlyContinue
+if ($chrome -eq $null) {
+    Start-Process "chrome" -ArgumentList "https://www.champlain.edu"
+    Write-Host "Chrome started... directed to www.champlain.edu"
+}
+else {
+    Stop-Process -Name chrome -Force
+    Write-Host "Chrome stopped"
+}
