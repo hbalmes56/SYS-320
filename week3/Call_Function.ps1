37491a0c0273e5049d797da2389f40ca34a1f39d
hbalmes56 <hadley-rae.balmes@mymail.champlain.edu>
2024-02-09 10:41:13 -0500

    Calling function from other code

---
+++ week3/Call_Function.ps1
@@ -0,0 +1,11 @@
+﻿. (Join-Path $PSScriptRoot "Functions_EventLogs.ps1")
+
+clear
+
+# login and logouts from last 15 days
+$loginoutsTable = LoginLogEvents 15
+$loginoutsTable
+
+# shutdowns and startups from last 25 days
+$shutandstartTable = SystemEventLogs 25
+$shutandstartTable
