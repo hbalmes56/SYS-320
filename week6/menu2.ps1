7ddd853f5f20e7c52e5bb7a2773db6c50cc27e39
hbalmes56 <hadley-rae.balmes@mymail.champlain.edu>
2024-03-08 08:50:51 -0500

    function for menu

---
+++ week6/menu2.ps1
@@ -0,0 +1,11 @@
+﻿function ApacheLogs10 {
+    $logs = "C:\xampp\apache\logs\access.log"
+
+    if (Test-Path $logs){
+        Get-Content $logs -Tail 10
+    }
+    else {
+        Write-Host "Log does not exist"
+    }
+
+}
